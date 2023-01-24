9991f2a3859f27a5090f7cc94229dc8edf44eb58
jefInfconalep2 <123207882+jefInfconalep2@users.noreply.github.com>
2023-01-24 14:44:01 -0600

    navbar y panel

---
+++ componentes/navbar.php
@@ -0,0 +1,31 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-success" >
+    <div class="container-fluid">
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+        </button>
+        <a class="navbar-brand" href="#" style="color: white;">CONALEP</a>
+        <div class="collapse navbar-collapse" id="navbarTogglerDemo03">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#" style="color: white;">Home</a>
+            </li>
+            <li class="nav-item">
+            <a class="nav-link" href="#" style="color: white;">Archivos</a>
+            </li>
+            <li class="nav-item">
+            <a class="nav-link" href="#" style="color: white;">Recursos</a>
+            </li>
+            <li class="nav-item">
+            <a class="nav-link" href="#"style="color: white;">Audiovisual</a>
+            </li>
+            <!-- <li class="nav-item">
+            <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+            </li> -->
+        </ul>
+        <form class="d-flex">
+            <input class="form-control me-2" type="search" placeholder="Ingresa tu busqueda" aria-label="Search">
+            <button class="btn btn-outline-success" type="submit" style="color: white;">Buscar</button>
+        </form>
+        </div>
+    </div>
+</nav>
